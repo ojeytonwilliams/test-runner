7b8109555abfae390890582fce677570c4eabffa
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-03-19 14:18:42 +0100

    fix: pick webpack mode

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,28 +1,34 @@
-module.exports = {
-	entry: "./src/index.ts",
-	output: {
-		filename: "bundle.mjs",
-		path: __dirname + "/dist",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				use: [
-					{
-						loader: "ts-loader",
-						options: {
-							compilerOptions: {
-								noEmit: false,
+module.exports = (env = {}) => {
+	const isDev = env.development;
+
+	return {
+		mode: isDev ? "development" : "production",
+		cache: isDev ? { type: "filesystem" } : false,
+		entry: "./src/index.ts",
+		output: {
+			filename: "bundle.mjs",
+			path: __dirname + "/dist",
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					use: [
+						{
+							loader: "ts-loader",
+							options: {
+								compilerOptions: {
+									noEmit: false,
+								},
 							},
 						},
-					},
-				],
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: [".ts", ".js"],
-	},
+					],
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
+	};
 };
