f1076074e7bbbb37c10ede9edfdbe508b6b9af02
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-03-26 14:52:45 +0100

    chore: ignore __fixtures__/dist dir when linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import globals from "globals";
 
 export default tseslint.config(
 	{
-		ignores: ["dist/**"],
+		ignores: ["dist/**", "__fixtures__/dist/**"],
 	},
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
